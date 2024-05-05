62e3a837dd19d42cdd42ba94abed255b48010b35
FabricioHiury <fabhiury@gmail.com>
2024-05-05 16:45:58 -0300

    Feat: crud companies

--- src/company/controllers/company.controller.ts
+++ src/company/controllers/company.controller.ts
@@ -1,7 +1,12 @@
 import {
   Body,
   Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
   Post,
+  Query,
   UseGuards,
   UsePipes,
   ValidationPipe,
@@ -11,6 +16,9 @@ import { RolesGuard } from 'src/control-roles/guards/roleGuards';
 import { CompanyService } from '../services/company.service';
 import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 import { CreateCompanyDto } from '../dto/create-company.dto';
+import { PaginationParamsDto } from 'src/pagination/pagination.dto';
+import { Role } from 'src/control-roles/decorators/roles.decorator';
+import { UpdateCompanyDto } from '../dto/update-company.dto';
 
 @ApiTags('Company')
 @Controller('company')
@@ -29,5 +37,39 @@ export class CompanyController {
   @ApiResponse({ status: 400, description: 'Bad Request' })
   async create(@Body() createCompanyDto: CreateCompanyDto) {
     return this.companyService.create(createCompanyDto);
+  }
+
+  @Role('ZICREDIT', 'SELLER', 'INVESTOR')
+  @Get()
+  async findAll(@Query() paginationParamsDto: PaginationParamsDto) {
+    return this.companyService.findAll(paginationParamsDto);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.companyService.findOne(id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Atualiza a companhia' })
+  @ApiResponse({
+    status: 200,
+    description: 'Companhia atualizada',
+    type: UpdateCompanyDto,
+  })
+  @ApiResponse({ status: 404, description: 'Companhia não encontrada' })
+  async update(
+    @Param('id') id: string,
+    @Body() updateCOmpanyDto: UpdateCompanyDto,
+  ) {
+    return this.companyService.update(id, updateCOmpanyDto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Remove uma companhia' })
+  @ApiResponse({ status: 200, description: 'Comapanhia removida' })
+  @ApiResponse({ status: 404, description: 'Companhia não encontrada' })
+  async remove(@Param('id') id: string) {
+    return this.companyService.remove(id);
   }
 }
